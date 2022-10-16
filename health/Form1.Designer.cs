e29423755d017cc0be61334e9262728de4d410e9
zkcl1012 <dlalsrl1011@naver.com>
2022-10-16 09:51:20 +0900

    kcal

---
+++ health/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace health
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.check_man = new System.Windows.Forms.CheckBox();
+            this.check_girl = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.all_kcal = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.subtract_kcal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // check_man
+            // 
+            this.check_man.AutoSize = true;
+            this.check_man.Font = new System.Drawing.Font("굴림", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.check_man.Location = new System.Drawing.Point(49, 61);
+            this.check_man.Name = "check_man";
+            this.check_man.Size = new System.Drawing.Size(71, 25);
+            this.check_man.TabIndex = 0;
+            this.check_man.Text = "남자";
+            this.check_man.UseVisualStyleBackColor = true;
+            // 
+            // check_girl
+            // 
+            this.check_girl.AutoSize = true;
+            this.check_girl.Font = new System.Drawing.Font("굴림", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.check_girl.Location = new System.Drawing.Point(49, 117);
+            this.check_girl.Name = "check_girl";
+            this.check_girl.Size = new System.Drawing.Size(71, 25);
+            this.check_girl.TabIndex = 1;
+            this.check_girl.Text = "여자";
+            this.check_girl.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(165, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 80);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "확인";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "팔굽혀펴기",
+            "윗몸일으키기",
+            "턱걸이",
+            "달리기",
+            "스쿼트"});
+            this.checkedListBox1.Location = new System.Drawing.Point(112, 323);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(113, 84);
+            this.checkedListBox1.TabIndex = 3;
+            this.checkedListBox1.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.checkedListBox1_ItemCheck);
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(376, 323);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(292, 29);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "1시간 총 소모 칼로리";
+            // 
+            // all_kcal
+            // 
+            this.all_kcal.AutoSize = true;
+            this.all_kcal.Font = new System.Drawing.Font("굴림", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.all_kcal.Location = new System.Drawing.Point(499, 382);
+            this.all_kcal.Name = "all_kcal";
+            this.all_kcal.Size = new System.Drawing.Size(58, 21);
+            this.all_kcal.TabIndex = 5;
+            this.all_kcal.Text = "label2";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(231, 323);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 80);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "확인";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(341, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 19);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "먹은 칼로리";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(468, 61);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(140, 21);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(626, 61);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "확인";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("굴림", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(308, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(143, 21);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "빼야할 칼로리";
+            // 
+            // subtract_kcal
+            // 
+            this.subtract_kcal.AutoSize = true;
+            this.subtract_kcal.Font = new System.Drawing.Font("굴림", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.subtract_kcal.Location = new System.Drawing.Point(513, 105);
+            this.subtract_kcal.Name = "subtract_kcal";
+            this.subtract_kcal.Size = new System.Drawing.Size(55, 19);
+            this.subtract_kcal.TabIndex = 13;
+            this.subtract_kcal.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.subtract_kcal);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.all_kcal);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.check_girl);
+            this.Controls.Add(this.check_man);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox check_man;
+        private System.Windows.Forms.CheckBox check_girl;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label all_kcal;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label subtract_kcal;
+    }
+}
+
---
+++ health/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace health
+{
+    public partial class Form1 : Form
+    {
+        public int m; //성인남자 평균 칼로리
+        public int g; //성인여자 평균 칼로리
+        public int a0 =0; //팔굽혀펴기 칼로리
+        public int a1=0; //윗몸일으키기 칼로리
+        public int a2=0; //턱걸이 칼로리
+        public int a3=0; //달리기 칼로리
+        public int a4=0; //스쿼트 칼로리
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(check_man.Checked == true)
+            {
+                m = 2500;
+            }
+            if(check_girl.Checked == true)
+            {
+                g = 1900;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            all_kcal.Text = ((a0 + a1 + a2 + a3 + a4).ToString());
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkedListBox1_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            if(checkedListBox1.CheckedItems.Count == 0)
+            {
+                a0 = 640;
+            }
+            if (checkedListBox1.CheckedItems.Count == 1)
+            {
+                a1 = 588;
+            }
+            if(checkedListBox1.CheckedItems.Count == 2)
+            {
+                a2 = 560;
+            }
+            if (checkedListBox1.CheckedItems.Count == 3)
+            {
+                a3 = 590;
+            }
+            if (checkedListBox1.CheckedItems.Count == 4)
+            {
+                a4 = 588;
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            string s = textBox1.Text; //하루 먹은 칼로리 입력받기
+            if (check_man.Checked == true)
+            {
+                subtract_kcal.Text = "= " + (Convert.ToInt32(s)-m).ToString();
+            }
+            if(check_girl.Checked == true)
+            {
+                subtract_kcal.Text = "= " + (Convert.ToInt32(s)-g).ToString();
+            }
+            
+
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void health_information_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.ShowDialog();
+        }
+    }
+}
